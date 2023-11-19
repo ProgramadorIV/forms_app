d792fd8781b7c52cf7c7dafd6b6577af80fe2075
Your Name <antoniojz1998@gmail.com>
2023-11-19 15:59:38 +0100

    register screen adapted to cubit

--- lib/presentation/screens/register_screen.dart
+++ lib/presentation/screens/register_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:forms_app/presentation/blocs/register_cubit/register_cubit.dart';
 import 'package:forms_app/presentation/widgets/widgets.dart';
 
 class RegisterScreen extends StatelessWidget {
@@ -10,7 +12,10 @@ class RegisterScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('New user'),
       ),
-      body: const _RegisterView(),
+      body: BlocProvider(
+        create: (context) => RegisterCubit(),
+        child: const _RegisterView(),
+      ),
     );
   }
 }
@@ -41,80 +46,44 @@ class _RegisterView extends StatelessWidget {
   }
 }
 
-class _RegisterForm extends StatefulWidget {
+class _RegisterForm extends StatelessWidget {
   const _RegisterForm();
 
   @override
-  State<_RegisterForm> createState() => _RegisterFormState();
-}
-
-class _RegisterFormState extends State<_RegisterForm> {
-  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
-  String username = '';
-  String password = '';
-  String email = '';
-
-  @override
   Widget build(BuildContext context) {
+    final registerCubit = context.watch<RegisterCubit>();
+    final usernameInput = registerCubit.state.username;
+    final passwordInput = registerCubit.state.password;
+    final emailInput = registerCubit.state.email;
+
     return Form(
-      key: _formKey,
       child: Column(
         children: [
           CustomTextFormField(
             label: 'Username',
             hintText: 'Enter your username',
-            onChanged: (value) => username = value,
-            validator: (value) {
-              if (value == null || value.isEmpty || value.trim().isEmpty) {
-                return 'Required field';
-              }
-
-              if (value.length < 6) {
-                return 'User must have more than 6 characters';
-              }
-
-              return null;
-            },
+            errorMessage: usernameInput.errorMessage,
+            onChanged: registerCubit.usernameChange,
           ),
           const SizedBox(height: 10),
           CustomTextFormField(
             label: 'Email',
             hintText: 'Enter your email',
-            onChanged: (value) => email = value,
-            validator: (value) {
-              if (value == null || value.isEmpty || value.trim().isEmpty) {
-                return 'Required field';
-              }
-
-              final emailRegExp = RegExp(r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$');
-              if (!email.contains(emailRegExp)) return 'No matched email';
-
-              return null;
-            },
+            errorMessage: emailInput.errorMessage,
+            onChanged: registerCubit.emailChange,
           ),
           const SizedBox(height: 10),
           CustomTextFormField(
             label: 'Password',
-            hintText: 'Enter your password',
-            onChanged: (value) => password = value,
-            validator: (value) {
-              if (value == null || value.isEmpty || value.trim().isEmpty) {
-                return 'Required field';
-              }
-
-              if (value.length < 6) {
-                return 'Password must have more than 6 characters';
-              }
-
-              return null;
-            },
             obscureText: true,
+            hintText: 'Enter your password',
+            errorMessage: passwordInput.errorMessage,
+            onChanged: registerCubit.passwordChange,
           ),
           const SizedBox(height: 20),
           FilledButton.tonalIcon(
             onPressed: () {
-              final isValid = _formKey.currentState!.validate();
-              if (!isValid) return;
+              registerCubit.onSubmit();
             },
             icon: const Icon(Icons.save),
             label: const Text('Register'),
